f190c0c5d92b0a146186543e73443e223aca2b78
Unknownymous <corgioraynor@live.com.ph>
2020-12-18 22:05:26 +0800

    update slider arrow and dots

--- slick.js
+++ slick.js
@@ -5,7 +5,11 @@ $(function () {
     centerPadding: '50px',
         slidesToShow: 3,
         arrows: true,
-    dots:true,
+        dots: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-chevron-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-chevron-right' aria-hidden='true'></i></button>",
+    // prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',
+    // nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',
     responsive: [
         {
         breakpoint: 768,
